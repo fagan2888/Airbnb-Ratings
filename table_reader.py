be076823a96ffc3edf8dd6caa00d20c0918f789a
Seth Peterson <seth@sethpeterson.net>
2019-08-06 18:10:10 -0400

    return table datas as pandas instead of with generators

--- table_reader.py
+++ table_reader.py
@@ -4,6 +4,8 @@
 import os
 import psycopg2
 import urllib.parse as up
+import pandas as pd
+import pandas.io.sql as sqlio
 
 
 class TableReader(object):
@@ -12,14 +14,14 @@ class TableReader(object):
 
         url = up.urlparse(os.environ["DATABASE_URL"])
 
-        conn = psycopg2.connect(database=url.path[1:],
+        self.conn = psycopg2.connect(database=url.path[1:],
                                 user=url.username,
                                 password=url.password,
                                 host=url.hostname,
                                 port=url.port
                                 )
 
-        self._cur = conn.cursor()
+        self._cur = self.conn.cursor()
 
     def ids(self):
         self._cur.execute('SELECT "listingID" FROM public."Properties"')
@@ -27,35 +29,32 @@ class TableReader(object):
             yield idx[0]
 
     def properties(self):
-        self._cur.execute(
-            'SELECT "listingID", property_type, room_type, accomodates, bathrooms, bedrooms, beds, bed_type, amenities, square_feet, price FROM public."Properties"')
-        for prop in iter(self._cur.fetchone, None):
-            yield prop
+        sql = 'SELECT "listingID", property_type, room_type, accomodates, bathrooms, bedrooms, beds, bed_type, amenities, square_feet, price FROM public."Properties";'
+        props = sqlio.read_sql_query(sql, self.conn)
+        return props;
 
     def descriptions(self):
-        self._cur.execute(
-            'SELECT "listingID", name, summary, space, description, neighborhood_overview, notes, transit, access, interaction, house_rules FROM public."Descriptions";')
-        for desc in iter(self._cur.fetchone, None):
-            yield desc
+        sql = 'SELECT "listingID", name, summary, space, description, neighborhood_overview, notes, transit, access, interaction, house_rules FROM public."Descriptions";'
+        descs = sqlio.read_sql_query(sql, self.conn)
+        return descs;
 
     def geodata(self):
-        self._cur.execute(
-            'SELECT "listingID", street, neighborhood, city, state, zipcode, smart_location, country, latitude, longitude FROM public."GeoData";')
-        for geo in iter(self._cur.fetchone, None):
-            yield geo
+        sql = 'SELECT "listingID", street, neighborhood, city, state, zipcode, smart_location, country, latitude, longitude FROM public."GeoData";'
+        geo = sqlio.read_sql_query(sql, self.conn)
+        return geo;
 
     def reviews(self):
-        self._cur.execute(
-            'SELECT "listingID", num_reviews, rating, accuracy, cleanliness, checkin, communication, location, value FROM public."Reviews";')
-        for rev in iter(self._cur.fetchone, None):
-            yield rev
+        sql = 'SELECT "listingID", num_reviews, rating, accuracy, cleanliness, checkin, communication, location, value FROM public."Reviews";'
+        revs = sqlio.read_sql_query(sql, self.conn)
+        return revs;
 
     def close(self):
         self._cur.close()
+        self.conn = None
 
 
 if __name__ == '__main__':
     rdr = TableReader()
-    for item in rdr.properties():
-        print(item)
+    df = rdr.properties();
+    print(df.head())
     rdr.close()
